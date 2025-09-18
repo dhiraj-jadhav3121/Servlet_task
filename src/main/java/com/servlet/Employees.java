e93933b1e0cda557d3b3d08cab9108eb3c52e09b
Dhiraj Jadhav <dhiraj.jadhav9767@gmail.com>
2025-09-18 13:17:03 +0530

    Changes in employees file

--- src/main/java/com/servlet/Employees.java
+++ src/main/java/com/servlet/Employees.java
@@ -2,10 +2,11 @@ package main.java.com.servlet;
 
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -16,8 +17,7 @@ import jakarta.servlet.http.HttpServletResponse;
 public class Employees extends HttpServlet {
 
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         String id = request.getParameter("empId");
         int empId = Integer.parseInt(id);
@@ -28,9 +28,6 @@ public class Employees extends HttpServlet {
 
         String age = request.getParameter("empAge");
         int empAge = Integer.parseInt(age);
-
-        String dob = request.getParameter("empDob");
-        int empDob = Integer.parseInt(age);
 
         String address = request.getParameter("empAddress");
 
@@ -46,20 +43,41 @@ public class Employees extends HttpServlet {
         String exp = request.getParameter("empExp");
         int empExp = Integer.parseInt(exp);
 
+        response.setContentType("application/json"); // Set response type to JSON
+        PrintWriter out = response.getWriter();
 
-        System.out.println(id);
-        System.out.println(name);
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/employees", "root", "Amol@1234");
+            PreparedStatement ps = c.prepareStatement(
+                    "insert into employee (id, name, email, age,address, state, gender, dept, salary, exp) " +
+                            "VALUES (?, ?, ?, ?, ?, ?, ?, ?,?, ?)"
+            );
 
-        System.out.println(email);
+            ps.setInt(1, empId);
+            ps.setString(2, name);
+            ps.setString(3, email);
+            ps.setInt(4, empAge);
+            ps.setString(5, address);
+            ps.setString(6, state);
+            ps.setString(7, gender);
+            ps.setString(8, dept);
+            ps.setInt(9, empSalary);
+            ps.setInt(10, empExp);
 
-        System.out.println(age);
-        System.out.println(dob);
-        System.out.println(address);
-        System.out.println(state);
-        System.out.println(gender);
-        System.out.println(dept);
-        System.out.println(salary);
-        System.out.println(exp);
+            int check = ps.executeUpdate();
+
+
+            if (check > 0) {
+                System.out.println("data inserted...");
+            } else {
+                System.out.println("data no inserted");            }
+
+            c.close();
+
+        } catch (Exception e) {
+            out.println("Error: " + e.getMessage());
+        }
 
 
     }
